c2be07e89003aa019500c779a0a042f1fa5e9531
doh-ha <101639449+doh-ha@users.noreply.github.com>
2025-11-24 21:09:14 +0900

    🔧 remove blog

--- src/data/Contact.data.ts
+++ src/data/Contact.data.ts
@@ -9,6 +9,6 @@ export const contacts: ContactItemData[] = [
   { icon: "📧", label: "Email", href: "mailto:hayeonlr@gmail.com" },
   { icon: "💼", label: "LinkedIn", href: "https://www.linkedin.com/in/hayeon-doh-45102a252/" },
   { icon: "🐙", label: "GitHub", href: "https://github.com/doh-ha" },
-  { icon: "🌍", label: "Tistory", href: "https://ardohtory.tistory.com/" },
-  { icon: "👀", label: "Velog", href: "https://velog.io/@ardoh/posts" },
+  // { icon: "🌍", label: "Tistory", href: "https://ardohtory.tistory.com/" },
+  // { icon: "👀", label: "Velog", href: "https://velog.io/@ardoh/posts" },
 ];
